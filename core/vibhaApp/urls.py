d834de7354bba57b30e4d728bc3ce22f4cec9c15
sachin sen <sachinrk835257@gmail.com>
2023-08-24 18:10:25 +0530

    efsdhj

--- core/vibhaApp/urls.py
+++ core/vibhaApp/urls.py
@@ -4,5 +4,14 @@ from vibhaApp import views
 
 urlpatterns = [
     path('', views.index , name="go to vibha App"),
-    path('about/',views.about, name="about page"),
+    path('about-us/',views.about, name="about page"),
+    path('contact-us/',views.contact, name="about page"),
+    path('aims/',views.aims, name="about page"),
+    path('events/',views.events, name="about page"),
+    path('members/',views.members, name="about page"),
+    path('membership/',views.membership, name="about page"),
+    path('our-members/',views.our_members, name="about page"),
+    path('our-team/',views.our_team, name="about page"),
+    path('photo-gallery/',views.photo_gallery, name="about page"),
+    path('registration/',views.registration, name="about page"),
 ]
